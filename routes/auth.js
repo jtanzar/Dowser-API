047854f5067b861074538818930ead6e583da335
Jamie Tanzar <jtanzar@gmail.com>
2017-11-21 15:12:47 -0700

    testing routes for much success

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const passport = require('passport')
 module.exports = (app) => {
 
 app.get('/auth/google', passport.authenticate('google', {
-  scope: ['profile']
+  scope: ['profile', 'email']
 }), (req, res) => {
   console.log('req user 1', req.user)
   res.send(req.user)
